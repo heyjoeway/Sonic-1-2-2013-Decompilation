580585ce74088faabfad3863a879fb762bacb741
Ash Logan <ash@heyquark.com>
2021-01-20 22:35:00 +1100

    wiiu: Add logging
    This isn't *strictly* needed but it sure is nice

--- Sonic12Decomp/RetroEngine.cpp
+++ Sonic12Decomp/RetroEngine.cpp
@@ -204,11 +204,34 @@ bool processEvents()
     return true;
 }
 
+#if __WIIU__
+#include <whb/log.h>
+#include <whb/log_udp.h>
+#include <whb/log_cafe.h>
+#include <unistd.h>
+#include <sys/iosupport.h>
+
+static ssize_t wiiu_log_write(struct _reent* r, void* fd, const char* ptr, size_t len) {
+    WHBLogPrintf("%*.*s", len, len, ptr);
+    return len;
+}
+static const devoptab_t dotab_stdout = {
+    .name = "stdout_whb",
+    .write_r = wiiu_log_write,
+};
+#endif
+
 #if RETRO_USE_NETWORKING && RSDK_DEBUG
 #include <string>
 #endif
 void RetroEngine::Init()
 {
+#if __WIIU__
+    WHBLogUdpInit();
+    WHBLogCafeInit();
+    devoptab_list[STD_OUT] = &dotab_stdout;
+    devoptab_list[STD_ERR] = &dotab_stdout;
+#endif
     CalculateTrigAngles();
     GenerateBlendLookupTable();
 
